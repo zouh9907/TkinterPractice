a2bb2cff97348cb75f205540ca52336eef7ea074
zouh <zouh@rose-hulman.edu>
2018-02-04 22:51:42 -0500

    finish

--- src/m5_tkinter_practice.py
+++ src/m5_tkinter_practice.py
@@ -12,37 +12,49 @@ from tkinter import ttk
 def main():
     """ Constructs a GUI with stuff on it. """
     # ------------------------------------------------------------------
-    # TODO: 2. After reading and understanding the m1e module,
+    # DONE: 2. After reading and understanding the m1e module,
     #   ** make a window that shows up. **
     # ------------------------------------------------------------------
+    root = tkinter.Tk()
 
     # ------------------------------------------------------------------
-    # TODO: 3. After reading and understanding the m2e module,
+    # DONE: 3. After reading and understanding the m2e module,
     #   ** put a Frame on the window. **
     # ------------------------------------------------------------------
-
+    frame1 = ttk.Frame(root, padding=100)
+    frame1.grid()
     # ------------------------------------------------------------------
-    # TODO: 4. After reading and understanding the m2e module,
+    # DONE: 4. After reading and understanding the m2e module,
     #   ** put a Button on the Frame. **
     # ------------------------------------------------------------------
-
+    go_forward_button = ttk.Button(frame1, text='Forward')
+    go_forward_button.grid()
     # ------------------------------------------------------------------
-    # TODO: 5. After reading and understanding the m3e module,
+    # DONE: 5. After reading and understanding the m3e module,
     #   ** make your Button respond to a button-press **
     #   ** by printing   "Hello"  on the Console.     **
     # ------------------------------------------------------------------
-
+    go_forward_button['command'] = (lambda:
+                                     print('Hello'))
     # ------------------------------------------------------------------
-    # TODO: 6. After reading and understanding the m4e module,
+    # DONE: 6. After reading and understanding the m4e module,
     #   -- Put an Entry box on the Frame.
     #   -- Put a second Button on the Frame.
     #   -- Make this new Button, when pressed, print "Hello"
     #        on the Console if the current string in the Entry box
     #        is the string 'ok', but print "Goodbye" otherwise.
     # ------------------------------------------------------------------
+    my_entry_box = ttk.Entry(frame1)
+    my_entry_box.grid()
+
+    go_ok_button = ttk.Button(frame1, text='test for ok')
+    go_ok_button['command'] = (lambda:
+    check(my_entry_box))
+    go_ok_button.grid()
+
 
     # ------------------------------------------------------------------
-    # TODO: 7.
+    # DONE: 7.
     #    -- Put a second Entry on the Frame.
     #    -- Put a third Button on the frame.
     #    -- Make this new Button respond to a button-press as follows:
@@ -64,11 +76,30 @@ def main():
     #      s = entry_box.get()
     #      n = int(s)
     ####################################################################
+    my_entry_box2 = ttk.Entry(frame1)
+    my_entry_box2.grid()
+
+    go_ok_button2 = ttk.Button(frame1, text='test for n')
+    go_ok_button2['command'] = (lambda:
+    print_n(my_entry_box2, my_entry_box))
+    go_ok_button2.grid()
 
     # ------------------------------------------------------------------
-    # TODO: 8. As time permits, do other interesting GUI things!
+    # DONE: 8. As time permits, do other interesting GUI things!
     # ------------------------------------------------------------------
+    root.mainloop()
 
+def check(enter):
+    contents = enter.get()
+    if contents == "ok":
+        print("Hello")
+
+
+def print_n(enter1, enter2):
+    s = enter1.get()
+    n = int(s)
+    for k in range(n):
+        print(enter2.get())
 
 # ----------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
